ec5e8337b8635f662de1d6d26ec5396d3e4d2873
Luciano Gomes <luciano@pontotel.com.br>
2022-07-27 18:33:03 -0300

    feat(src/components/NotificacoesAtividade.vue): adiciona componente de Notificações

---
+++ src/components/NotificacoesAtividade.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="notificacoes">
+        <article class="message is-primary">
+            <div class="message-header">
+                <p>Atenção !</p>
+                <button class="delete" aria-label="delete"></button>
+            </div>
+            <div class="message-body">
+                Texto de Notificação
+            </div>
+        </article>
+        <article class="message is-warning">
+            <div class="message-header">
+                <p>Atenção !</p>
+                <button class="delete" aria-label="delete"></button>
+            </div>
+            <div class="message-body">
+                Texto de Notificação
+            </div>
+        </article>
+        <article class="message is-danger">
+            <div class="message-header">
+                <p>Atenção !</p>
+                <button class="delete" aria-label="delete"></button>
+            </div>
+            <div class="message-body">
+                Texto de Notificação
+            </div>
+        </article>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+
+export default defineComponent({
+    name: 'NotificacoesAtividade'
+})
+</script>
+
+<style scoped>
+.notificacoes {
+    position: absolute;
+    right: 0;
+    width: 300px;
+    padding: 8px;
+    z-index: 105;
+}
+</style>
